a715577055e4e8573ad53caa4210041ee3ff9384
AndriottiGuZ <162466648+AndriottiGuZ@users.noreply.github.com>
2024-04-26 17:20:43 -0300

    att

--- src/Lista5/Lista5_2.java
+++ src/Lista5/Lista5_2.java
@@ -1,8 +1,41 @@
 package Lista5;
 
+import java.util.Random;
+
 public class Lista5_2 {
     public static void main(String[] args){
+
+
+        int[][] M = new int[6][8];
+        int[] C = new int[6];
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                M[i][j] = random.nextInt(21) - 10;
+            }
+        }
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (M[i][j] < 0) {
+                    C[i]++;
+                }
+            }
+        }
+
+        System.out.println("Matriz M:");
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nVetor C:");
+        for (int i = 0; i < 6; i++) {
+            System.out.print(C[i] + " ");
+        }
     }
 }
-
 
---
+++ src/Lista5/Lista5_3.java
@@ -0,0 +1,28 @@
+package Lista5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Lista5_3 {
+    public static void main(String[] args) {
+        int[] C = new int[5];
+        int[][] M = new int[2][5];
+        Random random = new Random();
+
+        for (int i = 0; i<2; i++){
+            for (int j = 0; j<5; j++){
+                M[i][j]=random.nextInt(20);
+            }
+        }
+
+        for (int i = 0; i<1; i++) {
+            for (int j = 0; j < 5; j++) {
+                C[j] = M[i][j] + M[i + 1][j];
+            }
+        }
+
+        for (int i = 0; i<5; i++){
+            System.out.printf("soma coluna %d [%d + %d = %d]\n",i,M[0][i],M[1][i],C[i]);
+        }
+    }
+}
