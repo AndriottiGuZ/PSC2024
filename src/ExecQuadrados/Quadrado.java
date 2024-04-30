eeef9da14a16c4ad8d1076c36494ed09386aaa97
Gustavo Machado Andriotti - 1292413089 <1292413089@ulife.com.br>
2024-04-29 21:05:11 -0300

    att

---
+++ src/ExecQuadrados/Quadrado.java
@@ -0,0 +1,28 @@
+package ExecQuadrados;
+
+public class Quadrado {
+
+    private double ladoDoQuadrado;
+
+    public double getLadoDoQuadrado() {
+        return ladoDoQuadrado;
+    }
+
+    public void setLadoDoQuadrado(double num) {
+        ladoDoQuadrado = num;
+    }
+
+    public double getAreaQuadrado(){
+    return getLadoDoQuadrado() * getLadoDoQuadrado();
+    }
+
+    public double getPerimetroQuadrado(){
+       return getLadoDoQuadrado() * 4;
+
+    }
+
+    public double getdiagonalQuadrado(){
+        return getLadoDoQuadrado() * 1.41;
+
+    }
+}
---
+++ src/ExecQuadrados/TestaQuadrado.java
@@ -0,0 +1,18 @@
+package ExecQuadrados;
+
+import java.util.Scanner;
+
+public class TestaQuadrado {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Quadrado quadrado = new Quadrado();
+
+        System.out.print("qual valor do lado do quadrado: ");
+        int valor = ler.nextInt();
+        quadrado.setLadoDoQuadrado(valor);
+
+        System.out.printf("A area do quadrado é: %.1f\n", quadrado.getAreaQuadrado());
+        System.out.printf("O perimetro do quadrado é: %.1f\n", quadrado.getPerimetroQuadrado());
+        System.out.printf("A diagonal do quadrado é: %.1f", quadrado.getdiagonalQuadrado());
+    }
+}
